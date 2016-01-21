e4d0bf43c1a6b9b53786fc6c75045ad881bb6e23
ojanacek <janacek.o@outlook.com>
2016-01-21 08:33:38 +0100

    Initial commit

---
+++ Genetic3SAT/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Genetic3SAT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Genetic3SAT")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("50fa1661-2e42-46fc-9e34-8cb77df48885")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,3 @@
+# 3SAT problem
+
+Genetic algorithm solution.
