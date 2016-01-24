e8487c5c67f3ec2406e7b5f19ebff33cda7f993d
ojanacek <janacek.o@outlook.com>
2016-01-24 10:37:00 +0100

    Adds weights generator.

---
+++ Genetic3SAT.WeightsGenerator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Genetic3SAT.WeightsGenerator
+{
+    class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No directory specified.");
+                Console.ReadLine();
+                return;
+            }
+            
+            foreach (var file in Directory.EnumerateFiles(args[0], "*.cnf"))
+            {
+                var lines = File.ReadAllLines(file).ToList();
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    var line = lines[i];
+                    if (line.StartsWith("c"))
+                        continue;
+
+                    if (line.StartsWith("p"))
+                    {
+                        var preambleValues = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                                                 .Skip(2)
+                                                 .Select(int.Parse)
+                                                 .ToArray();
+                        int variableCount = preambleValues[0];
+                        var weightsLine = $"w {string.Join(" ", GenerateRandomWeights(variableCount))}";
+
+                        lines.Insert(i, weightsLine);
+                        File.WriteAllLines(file, lines);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static int[] GenerateRandomWeights(int count)
+        {
+            var weights = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                weights[i] = random.Next(1, count + 1);
+            }
+            return weights;
+        }
+    }
+}
---
+++ Genetic3SAT.WeightsGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Genetic3SAT.WeightsGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Genetic3SAT.WeightsGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4b9e4e88-4ba7-4955-af52-4bfa82e9d31a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
